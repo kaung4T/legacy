94100d42990883b5d3500e541301a9ce3cc2919d
kaung4T <mrkaungminnkhant@gmail.com>
2024-10-20 11:05:49 +0630

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { WeekComponent } from '../chart/week/week.component';
 import { SaleComponent } from '../chart/sale/sale.component';
 import { ProfitComponent } from '../chart/profit/profit.component';
 import { ProductComponent } from '../chart/product/product.component';
 import { CustomerComponent } from '../chart/customer/customer.component';
 import { TargetComponent } from '../chart/target/target.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +16,15 @@ import { TargetComponent } from '../chart/target/target.component';
     ProfitComponent,
     ProductComponent,
     CustomerComponent,
-    TargetComponent
+    TargetComponent,
+    FormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
+
+  // data: string|null = "";
 
 }
